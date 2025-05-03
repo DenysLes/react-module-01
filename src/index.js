d03741dee72d0d027d2cdf280ede6b9c7ba4f5ad
DenysLes <157131711+DenysLes@users.noreply.github.com>
2025-05-03 21:18:30 +0300

    update

---
+++ src/index.js
@@ -0,0 +1,36 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+import React from "react";
+import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom/client";
+
+// import { render } from "react-dom";
+
+const element = React.createElement("div", {
+  a: 5,
+  b: 10,
+  children: ["Привіт", " ", "світ"],
+});
+
+console.log(element);
+
+// const domNode = document.getElementById("root");
+
+ReactDOM.render(element, document.querySelector("#root"));
