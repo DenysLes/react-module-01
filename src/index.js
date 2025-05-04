17110f899a61ade3630fe2b8a77ee8d63afc7121
DenysLes <157131711+DenysLes@users.noreply.github.com>
2025-05-04 23:40:01 +0300

    update.  @import font

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
-// import "./index.css";
+import './index.css';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
@@ -18,7 +18,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 const element = React.createElement('div', {
   a: 5,
   b: 10,
-  children: ['Привіт', ' ', 'світ', '!'],
+  children: ['Hello', ' ', 'world', ' ', '!'],
 });
 
 console.log(element);
