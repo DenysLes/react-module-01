4683336e8e42a4921b33e4b1a46068a743428b8c
DenysLes <157131711+DenysLes@users.noreply.github.com>
2025-05-04 21:38:11 +0300

    update render fine!)))

--- src/index.js
+++ src/index.js
@@ -11,20 +11,16 @@
 //   </React.StrictMode>
 // );
 
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 // import ReactDOM from "react-dom/client";
-const element = React.createElement("div", {
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const element = React.createElement('div', {
   a: 5,
   b: 10,
-  children: ["Привіт", " ", "світ"],
+  children: ['Привіт', ' ', 'світ', '!'],
 });
 
 console.log(element);
 
-ReactDOM.render(element, document.querySelector("#root"));
+root.render(element);
