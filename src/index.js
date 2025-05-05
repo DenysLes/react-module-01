32463ae54a643c6794c1fb56f5803d4ddfba2b7e
DenysLes <157131711+DenysLes@users.noreply.github.com>
2025-05-05 21:33:41 +0300

    update

--- src/index.js
+++ src/index.js
@@ -14,22 +14,19 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+const p = React.createElement('p', {
+  id: 'pId',
+  className: 'pClass',
+  children: ['This is paragraph .', <br />, 'Hello world ! ', 'My ', 'name ', 'is ', 'Den'],
+});
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const div = React.createElement(
-  'div',
-  {
-    id: 'el-test-Id',
-    className: 'el-test-class',
-    a: 5,
-    b: 10,
-  },
-  //children
-  'Hello world ! ',
-  'My ',
-  'name',
-  ' ',
-  'Den'
-);
+const div = React.createElement('div', {
+  id: 'el-test-Id',
+  className: 'el-test-class',
+  a: 5,
+  b: 10,
+  children: p,
+});
 
 root.render(div);
 
