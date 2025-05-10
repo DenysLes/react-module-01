86e55821c732d205ee497dd38543b337f2e106e1
DenysLes <157131711+DenysLes@users.noreply.github.com>
2025-05-10 22:10:33 +0300

    Update json and js file

--- src/index.js
+++ src/index.js
@@ -15,26 +15,26 @@
 import ReactDOM from 'react-dom/client';
 import data from './data.json';
 
-const card = data.map((recipe) => {
-  return (
-    <div className='card mx-auto my-2' style={{ width: '18rem' }}>
-      <img src={recipe.image} class='card-img-top' alt={recipe.name} />
-      <div className='card-body'>
-        {data[0].name}
-        <br></br>
-        <h5 className='card-title'>{recipe.calories} calories</h5>
-        <p className='card-text'>
-          Cooking time : {recipe.time}
+const Card = () => {
+  return data.map((recipe) => {
+    return (
+      <div key={recipe.id} className='card mx-auto my-2' style={{ width: '18rem' }}>
+        <img src={recipe.image} className='card-img-top' alt={recipe.name} />
+        <div className='card-body'>
+          {data[0].name}
           <br></br>
-          servings : {recipe.servings}
-        </p>
-        <a href='http' className='btn btn-primary'>
-          Go somewhere 😉
-        </a>
+          <h5 className='card-title'>{recipe.calories} calories</h5>
+          <p className='card-text'>Cooking time : {recipe.time}</p>
+          <br></br>
+          <p> servings : {recipe.servings}</p>
+          <a href='http' className='btn btn-primary'>
+            Go somewhere 😉
+          </a>
+        </div>
       </div>
-    </div>
-  );
-});
+    );
+  });
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(card);
+root.render(<Card />);
