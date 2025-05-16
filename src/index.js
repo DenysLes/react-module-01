8c30534b8c5f1489551be5f2e09c7adc4ec90418
DenysLes <157131711+DenysLes@users.noreply.github.com>
2025-05-16 18:13:21 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
 import ReactDOM from 'react-dom/client';
 import data from './data.json';
 
-const Card = () => {
-  return data.map((recipe) => {
-    return (
-      <div key={recipe.id} className='card mx-auto my-2' style={{ width: '18rem' }}>
-        <img src={recipe.image} className='card-img-top' alt={recipe.name} />
-        <div className='card-body'>
-          {data[0].name}
-          <br></br>
-          <h5 className='card-title'>{recipe.calories} calories</h5>
-          <p className='card-text'>Cooking time : {recipe.time}</p>
-          <br></br>
-          <p> servings : {recipe.servings}</p>
-          <a href='http' className='btn btn-primary'>
-            Go somewhere 😉
-          </a>
-        </div>
-      </div>
-    );
-  });
-};
+function Card(recipe) {
+  const { image, name, calories, time, servings } = recipe;
+  return (
+    <div>
+      <img src={image} alt={name} width='480' />
+      <h2>{name}</h2>
+      <p>{calories} calories </p>
+      <p>{time} min</p>
+      <p>{servings} servings </p>
+    </div>
+  );
+}
 
+const datas = data[0];
+
+// data.map((recipe) => { })
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Card />);
+root.render(
+  <Card
+    id={data.id}
+    image={datas.image}
+    name={datas.name}
+    calories={datas.calories}
+    time={datas.time}
+    servings={datas.servings}
+  />
+);
