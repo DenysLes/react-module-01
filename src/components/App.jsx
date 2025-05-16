33e7a054e0eec9a9a588bf65c3c04c6bda01c282
DenysLes <157131711+DenysLes@users.noreply.github.com>
2025-05-16 22:00:55 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,33 @@
-import Recipe from './Recipe';
-import data from '../data.json';
+import Recipe from "./Recipe";
+import data from "../data.json";
 
 export default function App() {
   return (
-    <Recipe
-      id={data.id}
-      image={data.image}
-      name={data.name}
-      calories={data.calories}
-      time={data.time}
-      servings={data.servings}
-    />
+    <div>
+      <Recipe
+        id={data[0].id}
+        image={data[0].image}
+        name={data[0].name}
+        calories={data[0].calories}
+        time={data[0].time}
+        servings={data[0].servings}
+      />
+      <Recipe
+        id={data[1].id}
+        image={data[1].image}
+        name={data[1].name}
+        calories={data[1].calories}
+        time={data[1].time}
+        servings={data[0].servings}
+      />
+      <Recipe
+        id={data[2].id}
+        image={data[2].image}
+        name={data[2].name}
+        calories={data[2].calories}
+        time={data[2].time}
+        servings={data[2].servings}
+      />
+    </div>
   );
 }
